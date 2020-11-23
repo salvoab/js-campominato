61ee8c51781a24b16d5fda54e183c019f2af5756
salvoab <salvatore.abate85@gmail.com>
2020-11-23 15:20:57 +0100

    Funzioni getRandomIntegerMinMax isInSequence
    funzione per generare numeri random, funzione per capire se un numero è in una sequenza, ciclo for di generazione dei 16 numeri casuali con test

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,38 @@
  * Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. 
 **********************************************************************************************************************************************/
 
+/**
+ * Generates a random integer number between a minimum and a maximum number (both included).
+ * 
+ * @param {number} min  minimum random number.
+ * @param {number} max  maximum random number.
+ * @return {number}     random number between min and max
+ */
+function getRandomIntegerMinMax(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isInSequence(sequence, item){
+    for(var i=0; i<sequence.length; i++){
+        if(sequence[i] === item){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+var numeriEsplosivi =[];
+for (var i=0; i<16; i++){
+    var numeroCasuale;
+    //continuo a generare un numero casuale fino a generare un numero che non è già presente in numeriEsplosivi
+    do{
+        numeroCasuale = getRandomIntegerMinMax(1,100);
+        //test
+        console.log(numeroCasuale);
+    } while(isInSequence(numeriEsplosivi, numeroCasuale));
+    //Inserisco il numero casuale non ripetuto nell'array numeriEsplosivi
+    numeriEsplosivi.push(numeroCasuale);
+}
+//test
+console.log(numeriEsplosivi);
