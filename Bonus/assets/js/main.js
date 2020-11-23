d4c93fde803a7f52a22751f3e4c1946070732a9e
salvoab <salvatore.abate85@gmail.com>
2020-11-23 16:57:46 +0100

    Creata cartella bonus e Inserito alert in caso di numero ripetuto
    Quando viene richiesto all'utente di inserire un numero, compare un alert se il numero appena inserito, era già stato inserito in precedenza

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -6,6 +6,12 @@
  * Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
  * La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
  * Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. 
+ * 
+ * BONUS
+ * all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+ * con difficoltà 0 => tra 1 e 100
+ * con difficoltà 1 =>  tra 1 e 80
+ * con difficoltà 2 => tra 1 e 50
 **********************************************************************************************************************************************/
 
 /**
@@ -42,14 +48,12 @@ function getRandomExplosiveNumbers(sequence, maxValue, sequenceLength){
     return sequence;
 }
 
-//Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
+//Il computer deve generare 16 numeri casuali tra 1 e maxNumber dipendente dalla difficoltà. I numeri non possono essere duplicati.
 var numeriEsplosivi = [];
 var maxNumber = 100;
 numeriEsplosivi = getRandomExplosiveNumbers(numeriEsplosivi, maxNumber, 16);
-//test
-//console.log(numeriEsplosivi);
 
-//Chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
+//Chiedere all’utente (maxNumber - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
 var numeriUtente = [];
 var punteggio = 0;
 var keepPlaying = true;
